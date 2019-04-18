21ae41d5ec29d228e5a2232299f6d3f8d0ad15c9
Jimmy S <jpsietsma@gmail.com>
2019-04-18 11:21:50 -0400

    Added core webpages project, added core console UI.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ConsoleUI
 {
     class Program
     {
+        public static IConfiguration Configuration;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var builder = new ConfigurationBuilder()
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            //Configuration = builder.Build();
+
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello Getting Started Group!");
+
+            //string connectionString = Configuration.GetConnectionString("Default");
+            //Console.WriteLine($@"Connection String: { connectionString }");
+
+            //string otherInfo = Configuration.GetSection("OtherInfo").Value;
+            //Console.WriteLine($@"Other info: { otherInfo }");
+
+            //string demo1 = Configuration.GetSection("Demos").GetSection("Demo1").Value;
+            //Console.WriteLine($@"Demo 1: { demo1 }");
+
+            //Console.ReadLine();
+
+
+            // ./Logs/LogFile.txt
+            string path = Path.Combine(".", "Logs");
+            string fullFile = Path.Combine(path, "LogFile.txt");
+
+            Directory.CreateDirectory(path);
+
+            List<string> logs = new List<string>
+            {
+                "This is my first log entry",
+                "This is my second log entry",
+                "Isn't he creative?"
+            };
+
+            File.WriteAllLines(fullFile, logs);
+
+            foreach (string log in File.ReadAllLines(fullFile))
+            {
+                Console.WriteLine(log);
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ RazorApp/Pages/Demo/MoreInfo.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorApp.Pages.Demo
+{
+    public class MoreInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
